5f0be864a4eac4f1be814b526dd28ebdc813d956
Alfredo <ajc7873@gmail.com>
2025-10-06 14:27:43 -0700

    Agregue otro test

--- test/volvercontigo_test.dart
+++ test/volvercontigo_test.dart
@@ -11,7 +11,11 @@ void main() {
   test('Hay 3 gotas de agua', () {
     expect(calculargotitas([5,0,1,2]), 3);
   });
-  test('Hay 3 gotas de agua', () {
+  test('Hay 7 gotas de agua', () {
     expect(calculargotitas([6,0,3,4,1,3]), 7);
   });
+  test('Hay 6 gotas de agua', () {
+    expect(calculargotitas([0,1,0,2,1,0,1,3,2,1,2,1]), 6);
+  });
 }
+

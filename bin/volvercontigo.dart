9ec1b2249e789eb9334671e35a26956cd6cc6679
Alfredo <ajc7873@gmail.com>
2025-10-06 13:18:27 -0700

    Cree el repositorio

---
+++ bin/volvercontigo.dart
@@ -0,0 +1,5 @@
+import 'package:volvercontigo/volvercontigo.dart' as volvercontigo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${volvercontigo.calculate()}!');
+}
---
+++ test/volvercontigo_test.dart
@@ -0,0 +1,8 @@
+import 'package:volvercontigo/volvercontigo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

7ddfb2b9c37615c98528addac64dcebcba85e5c2
Alfredo <ajc7873@gmail.com>
2025-10-06 14:04:19 -0700

    Cree los test

--- test/volvercontigo_test.dart
+++ test/volvercontigo_test.dart
@@ -2,7 +2,16 @@ import 'package:volvercontigo/volvercontigo.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('No hay agua', () {
+    expect(calculargotitas([]), 0);
+  });
+  test('No hay agua en un elemento', () {
+    expect(calculargotitas([5]), 0);
+  });
+  test('Hay 3 gotas de agua', () {
+    expect(calculargotitas([5,0,1,2]), 3);
+  });
+  test('Hay 3 gotas de agua', () {
+    expect(calculargotitas([6,0,3,4,1,3]), 7);
   });
 }

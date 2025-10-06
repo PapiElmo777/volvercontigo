882234d9deaefe48d1811542151ea7d473ba3bd0
Alfredo <ajc7873@gmail.com>
2025-10-06 14:17:52 -0700

    Agregue el ciclo que mueve los punteros

--- lib/volvercontigo.dart
+++ lib/volvercontigo.dart
@@ -1,14 +1,16 @@
 int calculargotitas(List<int> alturas) {
-  if (alturas.length < 3) {
+  if (alturas.length <= 2) {
     return 0;
   }
   int totalAgua = 0;
   int punteroIzquierdo = 0;
   int punteroDerecho = alturas.length - 1;
-
   int maximoIzquierdo = 0;
   int maximoDerecho = 0;
 
-  
+  while (punteroIzquierdo < punteroDerecho) {
+    break; 
+  }
 
+  return totalAgua;
 }
